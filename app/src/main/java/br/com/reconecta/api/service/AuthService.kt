b1da5abbfefee315ba74c2adc1dc18265f55f20f
LeoDev <leonardoiigd2013@gmail.com>
2023-11-15 09:48:23 -0300

    change auth route in AuthService

--- app/src/main/java/br/com/reconecta/api/service/AuthService.kt
+++ app/src/main/java/br/com/reconecta/api/service/AuthService.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
-    @POST("api/auth/token")
+    @POST("api/user/auth")
     fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
 }
