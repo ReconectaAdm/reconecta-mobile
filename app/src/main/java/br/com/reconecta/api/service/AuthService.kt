97e228b3c86bdc072ce48e3eee65ff1799249619
FrtsVictor <victorfreitas.job@gmail.com>
2023-11-15 12:01:27 -0300

    updated http verb to patch

--- app/src/main/java/br/com/reconecta/api/service/AuthService.kt
+++ app/src/main/java/br/com/reconecta/api/service/AuthService.kt
@@ -5,12 +5,13 @@ import br.com.reconecta.api.model.LoginResponse
 import br.com.reconecta.api.model.UpdatePasswordRequest
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("api/user/auth")
     fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
 
-    @POST("api/user/password")
+    @PATCH("api/user/password")
     fun updatePassword(@Body updatePasswordRequest: UpdatePasswordRequest): Call<Void>
 }
