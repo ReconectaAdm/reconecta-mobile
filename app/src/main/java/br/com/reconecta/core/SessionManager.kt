8c7a2fa1f2df17fad976da5410a3233f4f283fa3
FrtsVictor <victorfreitas.job@gmail.com>
2023-11-21 16:26:50 -0300

    added user to session

--- app/src/main/java/br/com/reconecta/core/SessionManager.kt
+++ app/src/main/java/br/com/reconecta/core/SessionManager.kt
@@ -3,6 +3,7 @@ package br.com.reconecta.core
 import android.content.Context
 import android.content.SharedPreferences
 import br.com.reconecta.R
+import br.com.reconecta.api.model.UserResponse
 
 class SessionManager(context: Context) {
     private var prefs: SharedPreferences =
@@ -10,6 +11,18 @@ class SessionManager(context: Context) {
 
     companion object {
         const val USER_TOKEN = "user_token"
+        const val USER_SESSION = "user_session"
+    }
+
+    fun saveUserSession(user: UserResponse){
+        val editor = prefs.edit()
+        editor.putString(USER_SESSION, createGsonSerializer().toJson(user))
+        editor.apply()
+    }
+
+    fun fetchUserInfo(): UserResponse? {
+        val sessionUser =  prefs.getString(USER_SESSION, null)
+        return sessionUser?.let { createGsonSerializer().fromJson(it, UserResponse::class.java) }
     }
 
     fun saveAuthToken(token: String) {
