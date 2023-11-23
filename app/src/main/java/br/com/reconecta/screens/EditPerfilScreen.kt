a8213aa987b322b2a1f61ccbb8e10cff847ea32b
FrtsVictor <victorfreitas.job@gmail.com>
2023-11-23 18:05:05 -0300

    added address

--- app/src/main/java/br/com/reconecta/screens/EditPerfilScreen.kt
+++ app/src/main/java/br/com/reconecta/screens/EditPerfilScreen.kt
@@ -1,4 +1,7 @@
 import android.content.Context
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -21,21 +24,32 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toFile
 import androidx.navigation.NavHostController
 import br.com.reconecta.R
-import br.com.reconecta.api.model.Address
+import br.com.reconecta.api.model.auth.Address
+import br.com.reconecta.api.model.auth.Company
+import br.com.reconecta.api.model.organization.UpdateOrganizationRequest
+import br.com.reconecta.api.service.IBaseCompanyService
+import br.com.reconecta.api.service.RetrofitFactory
+import br.com.reconecta.api.service.handleRetrofitApiCall
+import br.com.reconecta.components.EAccountType
 import br.com.reconecta.components.YesOrNoOption
 import br.com.reconecta.components.commons.Header
 import br.com.reconecta.components.commons.buttons.SecondaryButton
@@ -44,62 +58,92 @@ import br.com.reconecta.components.commons.text_field.masks.MaskVisualTransforma
 import br.com.reconecta.core.SessionManager
 import br.com.reconecta.enums.EScreenNames
 import br.com.reconecta.ui.theme.LightGreenReconecta
+import coil.compose.AsyncImage
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import retrofit2.http.Part
 
 @Composable
-fun EditPerfilScreen(applicationContext: Context, navController: NavHostController) {
+fun EditPerfilScreen(context: Context, navController: NavHostController) {
+    val addressFontSize = 13.sp
+    var retrofitService: IBaseCompanyService? = null
+    var selectedId by remember { mutableStateOf(0) }
     val showAddressModal = remember { mutableStateOf(false) }
-    val showExcludeAdressModal = remember { mutableStateOf(false) }
-    val email = remember { mutableStateOf("victor@teste.com") }
-    val npme = remember { mutableStateOf("Plas Recicla") }
-    val socialReason = remember { mutableStateOf("Plastt") }
-    val cnpj = remember { mutableStateOf("123321123321123") }
-    val telefone = remember { mutableStateOf("21968645987") }
-    val enderecos = remember {
-        mutableStateOf(
-            listOf(
-                Address(
-                    id = 1,
-                    state = "RJ",
-                    number = "12",
-                    postalCode = "25964-367",
-                    city = "Teresopolis",
-                    street = "Rua Yamatos"
-                ), Address(
-                    id = 1,
-                    state = "SP",
-                    number = "01",
-                    postalCode = "25964-367",
-                    city = "São Paulo",
-                    street = "Av Palmeiras do sul"
-                ), Address(
-                    id = 1,
-                    state = "MG",
-                    number = "054",
-                    postalCode = "25966-367",
-                    city = "Minas Gerais",
-                    street = "Rua do pão de queijo caro"
-                )
+    val showExcludeAddressModal = remember { mutableStateOf(false) }
+    val email = remember { mutableStateOf("") }
+    val name = remember { mutableStateOf("") }
+    val corporateReason = remember { mutableStateOf("") }
+    val cnpj = remember { mutableStateOf("") }
+    val phone = remember { mutableStateOf("") }
+    val addresses = remember { mutableStateOf(listOf<Address>()) }
+    val company = remember { mutableStateOf<Company?>(null) }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
+        }
+
+    fun asd(uri: Uri?) {
+        val file = uri?.let{
+            val file = it.toFile()
+            val filePart = MultipartBody.Part.createFormData(
+                "name", file.name,
+                file.asRequestBody("image/*".toMediaTypeOrNull())
             )
-        )
+
+            handleRetrofitApiCall(call = retrofitService?.uploadLogo(filePart)!!)
+        }
+
+
     }
+
+
+    SessionManager(context).fetchUserInfo()?.let {
+        email.value = it.email
+        name.value = it.company.name
+        corporateReason.value = it.company.corporateReason
+        cnpj.value = it.company.cnpj
+        phone.value = it.company.cnpj
+
+        retrofitService = if (it.company.type == EAccountType.ORGANIZATION) {
+            RetrofitFactory().getOrganizationService(context)
+        } else
+            RetrofitFactory().getEstablishmentService(context)
+    }
+
+
+
+    handleRetrofitApiCall(call = retrofitService?.getMe()!!, onResponse = {
+        if (it.isSuccessful) {
+            addresses.value = (it.body()!!.addresses)
+            company.value = it.body()!!
+        }
+    })
 
     when {
         showAddressModal.value -> {
-            AddAddressModal(showAddressModal)
+            AddAddressModal(
+                cancelAction = { showAddressModal.value = false },
+                context = context,
+                showModal = showAddressModal,
+                addresses = addresses
+            )
         }
 
-        showExcludeAdressModal.value -> {
-            RemoveAddressModal(showExcludeAdressModal)
+        showExcludeAddressModal.value -> {
+            RemoveAddressModal(cancelAction = { showExcludeAddressModal.value = false },
+                confirmAction = {
+                    handleRetrofitApiCall(call = RetrofitFactory().getAddressService(context)
+                        .deleteById(selectedId),
+                        onResponse = {
+                            if (it.isSuccessful) showExcludeAddressModal.value = false
+                        })
+                })
         }
     }
 
-    SessionManager(applicationContext).fetchUserInfo()?.let {
-        email.value = it.email
-        npme.value = it.company.name
-        socialReason.value = it.company.corporateReason
-        cnpj.value = it.company.cnpj
-        telefone.value = it.company.cnpj
-    }
+
 
     Column(
         modifier = Modifier.padding(horizontal = 30.dp),
@@ -108,13 +152,29 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
         Header(text = "Editar perfil") { navController.navigate(EScreenNames.ACCOUNT_INFO.path) }
 
         Row {
-            Image(
-                painter = painterResource(id = R.drawable.no_image),
-                contentDescription = "Logo",
-                modifier = Modifier.size(80.dp)
-            )
+            imageUri?.let {
+                AsyncImage(
+                    model = imageUri,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .height(80.dp)
+                        .width(80.dp)
+                        .clip(RoundedCornerShape(80.dp)),
+                    contentScale = ContentScale.Crop,
+                )
+            }
 
-            Row(Modifier.offset(-12.dp, 35.dp)) {
+            if (imageUri == null) {
+                Image(
+                    painter = painterResource(id = R.drawable.no_image),
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(80.dp),
+
+                    )
+            }
+
+            Row(Modifier.offset((-12).dp, 35.dp)) {
                 Box(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier
@@ -127,7 +187,9 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
                         painter = painterResource(id = R.drawable.edit_image_icon),
                         contentDescription = "Edit image icon",
                         tint = Color.White,
-                        modifier = Modifier.size(10.dp)
+                        modifier = Modifier
+                            .size(10.dp)
+                            .clickable { launcher.launch("image/*") },
                     )
                 }
             }
@@ -135,8 +197,8 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
         }
 
         BaseTextField(text = email, "E-mail", enabled = false)
-        BaseTextField(text = npme, "Nome", enabled = false)
-        BaseTextField(text = socialReason, "Razao Social", enabled = false)
+        BaseTextField(text = name, "Nome", enabled = false)
+        BaseTextField(text = corporateReason, "Razao Social", enabled = false)
         BaseTextField(
             text = cnpj,
             "Cnpj",
@@ -144,9 +206,9 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
             visualTransformation = MaskVisualTransformation("##.###.###/####-##")
         )
         BaseTextField(
-            text = telefone,
+            text = phone,
             "Telefone",
-            enabled = false,
+            enabled = true,
             visualTransformation = MaskVisualTransformation("(##)#####-####")
         )
 
@@ -166,7 +228,7 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
 
         LazyColumn(Modifier.fillMaxWidth()) {
 
-            items(items = enderecos.value, itemContent = {
+            items(items = addresses.value, itemContent = {
                 Spacer(modifier = Modifier.height(12.dp))
                 Row {
                     Icon(
@@ -178,7 +240,7 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
                     Text(
                         text = "${it.street}, ${it.number}, ${it.state}",
                         fontFamily = FontFamily(Font(R.font.poppins_light)),
-                        fontSize = 10.sp
+                        fontSize = addressFontSize
                     )
                 }
                 Row(
@@ -189,14 +251,17 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
                 ) {
                     Text(
                         text = "CEP:${it.postalCode}",
-                        fontSize = 10.sp,
+                        fontSize = addressFontSize,
                         fontFamily = FontFamily(Font(R.font.poppins_light))
                     )
-                    Row(modifier = Modifier.clickable { showExcludeAdressModal.value = true }) {
+                    Row(modifier = Modifier.clickable {
+                        showExcludeAddressModal.value = true
+                        selectedId = it.id!!
+                    }) {
                         Text(
                             text = "Excluir",
                             color = Color.Red,
-                            fontSize = 10.sp,
+                            fontSize = addressFontSize,
                             fontFamily = FontFamily(Font(R.font.poppins_light))
                         )
                         Icon(
@@ -209,22 +274,42 @@ fun EditPerfilScreen(applicationContext: Context, navController: NavHostControll
                 }
 
 
-                if (enderecos.value.lastIndex == enderecos.value.indexOf(it)) {
-                    SecondaryButton(text = "Salvar", onClick = {})
-
-                    CancelButton {}
+                if (addresses.value.lastIndex == addresses.value.indexOf(it)) {
+                    Spacer(Modifier.height(15.dp))
+                    Row(Modifier.fillMaxWidth(), Arrangement.Center) {
+                        SecondaryButton(text = "Salvar", onClick = {
+                            handleRetrofitApiCall(
+                                call = RetrofitFactory().getOrganizationService(context).updateById(
+                                    id = company.value?.id!!,
+                                    updateOrganizationRequest = UpdateOrganizationRequest(
+                                        name = name.value,
+                                        corporateReason = corporateReason.value,
+                                        cnpj = cnpj.value,
+                                        phone = phone.value,
+                                        description = "",
+                                        addresses = addresses.value
+                                    )
+                                )
+                            )
+                        })
+                    }
+                    Row(Modifier.fillMaxWidth(), Arrangement.Center) {
+                        CancelButton {}
+                    }
                 }
             })
-
         }
     }
-
-
 }
 
 
 @Composable
-private fun AddAddressModal(isOpenDialialog: MutableState<Boolean>) {
+private fun AddAddressModal(
+    showModal: MutableState<Boolean>,
+    cancelAction: () -> Unit,
+    context: Context,
+    addresses: MutableState<List<Address>>
+) {
     val street = remember { mutableStateOf("") }
     val number = remember { mutableStateOf("") }
     val city = remember { mutableStateOf("") }
@@ -258,22 +343,63 @@ private fun AddAddressModal(isOpenDialialog: MutableState<Boolean>) {
                     text = state, "Estado", isRequired = true, modifier = Modifier.width(60.dp)
                 )
                 Spacer(modifier = Modifier.width(10.dp))
-                BaseTextField(text = cep, "Cep")
+                BaseTextField(
+                    text = cep,
+                    "Cep",
+                    keyboardType = KeyboardType.Number,
+                    visualTransformation = MaskVisualTransformation("#####-###")
+                )
             }
         }
     }, onDismissRequest = {}, confirmButton = {
-        SecondaryButton("Adicionar", onClick = {})
-        CancelButton {
-            isOpenDialialog.value = false
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()
+        ) {
+            val addressToAdd = mutableListOf(
+                Address(
+                    state = state.value,
+                    number = number.value,
+                    city = city.value,
+                    street = street.value,
+                    postalCode = cep.value
+                )
+            )
+            SecondaryButton("Adicionar",
+                enabled = (validateStringState(state) && validateStringState(
+                    number,
+                    1
+                ) && validateStringState(city, 4) && validateStringState(
+                    street,
+                    4
+                ) && validateStringState(cep, 8)),
+                onClick = {
+                    handleRetrofitApiCall(call = RetrofitFactory().getAddressService(context = context)
+                        .addAddress(addressToAdd), onResponse = {
+                        if (it.isSuccessful) {
+                            showModal.value = false
+                            addressToAdd.addAll(addresses.value)
+                            addresses.value = addressToAdd
+                        }
+                    })
+
+
+                })
+            Spacer(modifier = Modifier.height(10.dp))
+            CancelButton { cancelAction() }
         }
     })
 }
 
+private fun validateStringState(str: MutableState<String>, size: Int? = 2) =
+    str.value.length >= size!!
+
 @Composable
-fun RemoveAddressModal(isOpenDialialog: MutableState<Boolean>) {
+fun RemoveAddressModal(
+    cancelAction: () -> Unit, confirmAction: () -> Unit
+) {
     AlertDialog(containerColor = Color.White, title = {
         Text(text = "Tem certeza que deseja excluir este endereço?")
     }, text = {
-        YesOrNoOption(cancelAction = { isOpenDialialog.value = false }, confirmAction = {})
-    }, onDismissRequest = {}, confirmButton = {})
+        YesOrNoOption(cancelAction = { cancelAction() }, confirmAction = { confirmAction() })
+    }, onDismissRequest = {}, confirmButton = { confirmAction() })
 }
